a5f685bfdd042862f0eac787552e6f555e8a1d44
Rin Iwai <me@eyrin.jp>
2026-06-28 23:31:30 +0900

    Initial Commit

---
+++ Miru/MiruApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiruApp.swift
+//  Miru
+//
+//  Created by rin on 2026/06/28.
+//
+
+import SwiftUI
+
+@main
+struct MiruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
